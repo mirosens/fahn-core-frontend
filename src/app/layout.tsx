95712b563f18827106eb424270d199176b5f6238
Igor Mirosenski <igor.mirosenski@polizei-bw.de>
2025-12-02 17:23:31 +0100

    Update favicon and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Fahndungsportal BW – Polizei Baden-Württemberg",
   description: "Die Polizei Baden-Württemberg bittet um Ihre Mithilfe.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
